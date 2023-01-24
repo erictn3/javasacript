8e631879a6da44a68dc2fbaecc5513f58d3849e4
Eric <erictaynguyen@gmail.com>
2023-01-24 13:36:43 -0800

    app function in app.js

--- meditation-app-master/app.js
+++ meditation-app-master/app.js
@@ -1,5 +1,18 @@
 const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play')
+    const outline = document.querySelector('.moving-outline circle')
+    const video = document.querySelector('.vid-container video')
 
+    // sounds
+    const sounds = document.querySelectorAll('.sound-picker button')
+    // time display
+    const timeDisplay = document.querySelector('.time-display')
+    // get length of outline
+    const outlineLength = outline.getTotalLength();
+
+    let fakeDuration = 600;
+    console.log(outlineLength)
 }
 
 
