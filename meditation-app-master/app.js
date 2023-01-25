f0a0bfe38a26e1e57afa71c26bd06c05c59bb3ec
Eric <erictaynguyen@gmail.com>
2023-01-25 15:36:07 -0800

    if else statement for play button

--- meditation-app-master/app.js
+++ meditation-app-master/app.js
@@ -10,8 +10,31 @@ const app = () => {
     const timeDisplay = document.querySelector('.time-display')
     // get length of outline
     const outlineLength = outline.getTotalLength();
-
+    // duration
     let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+
+    outline.style.strokeDashoffset = outlineLength;
+
+    // play sound 
+    play.addEventListener('click', () => {
+        checkPlaying(song)
+    })
+
+    // create a function to specific to stop and play the sounds
+    const checkPlaying = song => {
+        if (song.paused) {
+            video.play();
+            song.play();
+            play.src = './svg/pause.svg'
+        }
+        else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg'
+        }
+    }
     console.log(outlineLength)
 }
 
