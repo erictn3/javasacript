64bf499cffc6784d9619683b0a4789dbe9aeb0f1
Eric <erictaynguyen@gmail.com>
2023-02-21 11:21:30 -0800

    add stop feature

--- meditation-app-master/app.js
+++ meditation-app-master/app.js
@@ -59,6 +59,13 @@ const app = () => {
 
         // Animate the text
         timeDisplay.textContent = `${minutes}:${seconds}`
+
+        if (currentTime >= fakeDuration) {
+            song.pause()
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
     }
 
 }
