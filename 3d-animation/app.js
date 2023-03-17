4f4cb95e3715eb8c7bc12e05fe04a68cc450b08c
Eric <erictaynguyen@gmail.com>
2023-03-17 13:40:08 -0700

    moving animation event

---
+++ 3d-animation/app.js
@@ -0,0 +1,13 @@
+// Movement Animation to happen
+
+const card = document.querySelector('.card');
+const container = document.querySelector('.container')
+
+// Moving Animation Event
+container.addEventListener('mousemove', (e) => {
+    console.log('hey')
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+});
