25cfbf1b23bb70b1c1b1e6d355d572bac03e3167
Eric <erictaynguyen@gmail.com>
2023-03-20 13:50:29 -0700

    setting up players & position

--- 4-fighting-game/index.js
+++ 4-fighting-game/index.js
@@ -4,3 +4,52 @@ const c = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+// used object oriented programming
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity
+    }
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+enemy.draw();
+player.draw();
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+}
+
+animate();
