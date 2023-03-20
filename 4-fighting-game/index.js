33f923c269cdbd4bf98ac292612ffb2469c08b95
Eric <erictaynguyen@gmail.com>
2023-03-20 14:06:31 -0700

    adding velocity

--- 4-fighting-game/index.js
+++ 4-fighting-game/index.js
@@ -9,14 +9,28 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 // used object oriented programming
 
+const gravity = 0.2;
+
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position;
-        this.velocity = velocity
+        this.velocity = velocity;
+        this.height = 150
     }
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+
+        this.draw();
+
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else this.velocity.y += gravity;
     }
 
 }
@@ -28,7 +42,7 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
@@ -43,13 +57,14 @@ const enemy = new Sprite({
     }
 })
 
-enemy.draw();
-player.draw();
-
 console.log(player)
 
 function animate() {
-    window.requestAnimationFrame(animate)
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
 }
 
 animate();
